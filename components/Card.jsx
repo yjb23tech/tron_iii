93bd3b025000a31997c3e131d84e001453c3494d
yjb23tech <yjb23tech@gmail.com>
2024-01-24 12:59:22 +0000

    profit

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,17 +6,17 @@ const Card = (props) => {
     return (
         <div className="album-card">
             <img src={`images/${props.album_card_song_cover_art}`} />
-            <div className="album-card-contents">
+            <div className="album-card-contents util-box-border-blue">
                 <h2>{props.album_card_song_name}</h2>
-                <div className="info-group">
+                <div className="info-group util-box-border-red">
                     <img src={album_icon} />
                     <p>{props.album_card_album_name}</p>
                 </div>
-                <div className="info-group">
+                <div className="info-group util-box-border-red">
                     <img src={artist_icon} />
                     <p>{props.album_card_artist_name}</p>
                 </div>
-                <div className="info-group">
+                <div className="info-group util-box-border-red">
                     <img src={genre_icon} />
                     <p>{props.album_card_genre_name}</p>
                 </div>
